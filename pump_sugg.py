762476208b92865a805ae6466ae9aec9551be9ba
sebumatt <122722973+sebumatt@users.noreply.github.com>
2023-11-26 15:44:24 +0200

    pump_sugg.py

--- pump_sugg.py
+++ pump_sugg.py
@@ -4,6 +4,9 @@ import openai
 def generate_solution(head, flow, material, description):
     # Combine the inputs into a prompt for the AI
     prompt = f"Head: {head}\nFlow: {flow}\nMaterial: {material}\nDescription: {description}\n\nSuggest a suitable pump solution:"
+
+# Load the API key from an environment variable
+openai.api_key = os.getenv('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')
     
     # Call to OpenAI's API (replace 'YOUR_API_KEY' with your actual API key)
     response = openai.Completion.create(
